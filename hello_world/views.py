ccd52df467cb55c3b7a22e10eeff5b0fdd1632d9
tester <elciak82@gmail.com>
2018-04-07 14:11:35 +0200

    Zmiana imienia na 'elle'

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Ewelina"
+moje_imie = "elle"
 msg = "Hello World!"
 
 
--- heroku-cli-v6.16.8-ae149be-linux-x64/bin/heroku.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-const {run} = require('@cli-engine/engine')
-const config = {
-  channel: 'stable',
-  version: '6.16.8-ae149be'
-}
-run(process.argv, config)
